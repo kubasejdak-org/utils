81a1e4c8e759eb22d458078813898a0e992d04e5
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-29 17:10:14 +0100

    Use CONAN wrapper.

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -4,3 +4,15 @@ if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
 endif ()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+macro(conan_get)
+    conan_cmake_run(
+        REQUIRES            ${ARGN}
+        BUILD_TYPE          ${CMAKE_BUILD_TYPE}
+        PROFILE             ${CONAN_PROFILE}
+        BUILD               missing
+        GENERATORS          cmake_find_package cmake_paths
+    )
+
+    include(${CMAKE_CURRENT_BINARY_DIR}/conan_paths.cmake)
+endmacro ()
