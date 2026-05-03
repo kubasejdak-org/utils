c1f05852eacc119809e10b4a8201895822c62cb8
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-05-03 22:52:06 +0200

    Remove obsolete files

--- tools/ci/program-openocd.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import subprocess
-import sys
-
-cmd = ["openocd", "-f", "/usr/share/openocd/scripts/board/{}.cfg".format(sys.argv[1]), "-c", "program {} verify reset exit".format(sys.argv[2])]
-print(" ".join(cmd))
-
-subprocess.Popen(cmd, stdout=subprocess.PIPE)
