3d83a87964b5ca05f7d72e4efdf30610e4d35390
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-05-17 11:09:56 +0200

    Refactor logger tests to use spdlog and improve test descriptions

--- tests/Logger.cpp
+++ tests/Logger.cpp
@@ -29,45 +29,80 @@
 #include <utils/logger/Logger.hpp>
 
 #include <catch2/catch_test_macros.hpp>
-#include <fmt/printf.h>
+#include <spdlog/spdlog.h>
 
 REGISTER_LOGGER(Demo1Logger, "DEMO1", spdlog::level::off);
 REGISTER_LOGGER(Demo2Logger, "DEMO2", spdlog::level::critical);
 REGISTER_LOGGER(Demo3Logger, "DEMO3", spdlog::level::trace);
 REGISTER_LOGGER(Demo4Logger, "DEMO4", spdlog::level::info);
 
-TEST_CASE("1. Log messages are displayed correctly", "[unit][Logger]")
+TEST_CASE("1. OFF level logger produces no output and has no underlying instance", "[unit][Logger]")
 {
-    fmt::print("\n>>>>> Using explicitly set 'OFF' level at construction time. <<<<<\n");
+    CHECK(Demo1Logger::get() == nullptr);
+
     Demo1Logger::trace("This trace log shouldn't be visible");
     Demo1Logger::debug("This debug log shouldn't be visible");
     Demo1Logger::info("This info log shouldn't be visible");
     Demo1Logger::warn("This warn log shouldn't be visible");
     Demo1Logger::error("This error log shouldn't be visible");
     Demo1Logger::critical("This critical log shouldn't be visible");
+}
 
-    fmt::print("\n>>>>> Using explicitly set 'INFO' level at runtime. <<<<<\n");
+TEST_CASE("2. Logger instances have correct initial state", "[unit][Logger]")
+{
+    SECTION("2.1. CRITICAL level")
+    {
+        REQUIRE(Demo2Logger::get() != nullptr);
+        CHECK(Demo2Logger::get()->name() == "DEMO2");
+        CHECK(Demo2Logger::get()->level() == spdlog::level::critical);
+
+        Demo2Logger::trace("This trace log shouldn't be visible");
+        Demo2Logger::debug("This debug log shouldn't be visible");
+        Demo2Logger::info("This info log shouldn't be visible");
+        Demo2Logger::warn("This warn log shouldn't be visible");
+        Demo2Logger::error("This error log shouldn't be visible");
+        Demo2Logger::critical("This critical log will be visible");
+    }
+
+    SECTION("2.2. TRACE level")
+    {
+        REQUIRE(Demo3Logger::get() != nullptr);
+        CHECK(Demo3Logger::get()->name() == "DEMO3");
+        CHECK(Demo3Logger::get()->level() == spdlog::level::trace);
+
+        Demo3Logger::trace("This trace log will be visible");
+        Demo3Logger::debug("This debug log will be visible");
+        Demo3Logger::info("This info log will be visible");
+        Demo3Logger::warn("This warn log will be visible");
+        Demo3Logger::error("This error log will be visible");
+        Demo3Logger::critical("This critical log will be visible");
+    }
+
+    SECTION("2.3. INFO level")
+    {
+        REQUIRE(Demo4Logger::get() != nullptr);
+        CHECK(Demo4Logger::get()->name() == "DEMO4");
+        CHECK(Demo4Logger::get()->level() == spdlog::level::info);
+
+        Demo4Logger::trace("This trace log shouldn't be visible");
+        Demo4Logger::debug("This debug log shouldn't be visible");
+        Demo4Logger::info("This info log will be visible");
+        Demo4Logger::warn("This warn log will be visible");
+        Demo4Logger::error("This error log will be visible");
+        Demo4Logger::critical("This critical log will be visible");
+    }
+}
+
+TEST_CASE("3. Log level can be changed at runtime", "[unit][Logger]")
+{
+    REQUIRE(Demo2Logger::get() != nullptr);
     Demo2Logger::get()->set_level(spdlog::level::info);
+    CHECK(Demo2Logger::get()->level() == spdlog::level::info);
+
     Demo2Logger::trace("This trace log shouldn't be visible");
     Demo2Logger::debug("This debug log shouldn't be visible");
     Demo2Logger::info("This info log will be visible");
     Demo2Logger::warn("This warn log will be visible");
     Demo2Logger::error("This error log will be visible");
     Demo2Logger::critical("This critical log will be visible");
-
-    fmt::print("\n>>>>> Using explicitly set 'TRACE' level at construction time. <<<<<\n");
-    Demo3Logger::trace("This trace log will be visible");
-    Demo3Logger::debug("This debug log will be visible");
-    Demo3Logger::info("This info log will be visible");
-    Demo3Logger::warn("This warn log will be visible");
-    Demo3Logger::error("This error log will be visible");
-    Demo3Logger::critical("This critical log will be visible");
-
-    fmt::print("\n>>>>> Using explicitly set 'INFO' level at construction time. <<<<<\n");
-    Demo4Logger::trace("This trace log shouldn't be visible");
-    Demo4Logger::debug("This debug log shouldn't be visible");
-    Demo4Logger::info("This info log shouldn't be visible");
-    Demo4Logger::warn("This warn log shouldn't be visible");
-    Demo4Logger::error("This error log shouldn't be visible");
-    Demo4Logger::critical("This critical log will be visible");
 }
