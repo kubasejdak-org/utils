703070f5be2bbb279664d5b569bd9e6950c42df4
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-05-31 22:43:11 +0200

    Adjust global registry

--- tests/GlobalRegistry.cpp
+++ tests/GlobalRegistry.cpp
@@ -2,31 +2,27 @@
 ///
 /// @file
 /// @author Kuba Sejdak
-/// @copyright BSD 2-Clause License
+/// @copyright MIT License
 ///
-/// Copyright (c) 2020-2023, Kuba Sejdak <kuba.sejdak@gmail.com>
-/// All rights reserved.
+/// Copyright (c) 2020 Kuba Sejdak (kuba.sejdak@gmail.com)
 ///
-/// Redistribution and use in source and binary forms, with or without
-/// modification, are permitted provided that the following conditions are met:
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
 ///
-/// 1. Redistributions of source code must retain the above copyright notice, this
-///    list of conditions and the following disclaimer.
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
 ///
-/// 2. Redistributions in binary form must reproduce the above copyright notice,
-///    this list of conditions and the following disclaimer in the documentation
-///    and/or other materials provided with the distribution.
-///
-/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-/// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-/// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-/// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-/// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
 ///
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -35,20 +31,22 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <cstddef>
+#include <format>
 #include <memory>
+#include <string>
 #include <type_traits>
 
 TEST_CASE("1. All instances are correctly stored in GlobalRegistry with custom id type", "[unit][GlobalRegistry]")
 {
     struct Test {
-        explicit Test(int a)
+        explicit Test(unsigned int a)
             : value(a)
         {}
 
-        int value{};
+        unsigned int value{};
     };
 
-    using TestRegistry = utils::registry::GlobalRegistry<Test, int>;
+    using TestRegistry = utils::registry::GlobalRegistry<Test, unsigned int>;
     std::size_t instancesCount{};
 
     SECTION("1.1. 1 instance")
@@ -72,25 +70,26 @@ TEST_CASE("1. All instances are correctly stored in GlobalRegistry with custom i
 
     auto size = TestRegistry::size();
     REQUIRE(size == instancesCount);
-    for (std::size_t i = 0; i < size; ++i) {
-        auto instance = TestRegistry::get(int(i));
-        REQUIRE(std::is_same_v<decltype(instance), std::shared_ptr<Test>>);
-        REQUIRE(instance->value == int(i));
+    for (unsigned int i = 0; i < size; ++i) {
+        auto instance = TestRegistry::get(i);
+        static_assert(std::is_same_v<decltype(instance), std::shared_ptr<Test>>);
+        REQUIRE(instance != nullptr);
+        CHECK(instance->value == i);
     }
 
     constexpr int cInvalidId = 100;
     auto nullInstance = TestRegistry::get(cInvalidId);
-    REQUIRE(!nullInstance);
+    CHECK(nullInstance == nullptr);
 
     TestRegistry::clear();
     size = TestRegistry::size();
-    REQUIRE(size == 0);
+    CHECK(size == 0);
 }
 
 TEST_CASE("2. Move only types can be stored in GlobalRegistry with default id type", "[unit][GlobalRegistry]")
 {
     struct Test {
-        explicit Test(int a)
+        explicit Test(unsigned int a)
             : value(a)
         {}
 
@@ -107,7 +106,7 @@ TEST_CASE("2. Move only types can be stored in GlobalRegistry with default id ty
         Test& operator=(const Test&) = delete;
         Test& operator=(Test&&) = delete;
 
-        int value{};
+        unsigned int value{};
         bool moved{false};
     };
 
@@ -135,19 +134,20 @@ TEST_CASE("2. Move only types can be stored in GlobalRegistry with default id ty
 
     auto size = TestRegistry::size();
     REQUIRE(size == instancesCount);
-    for (std::size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         auto instance = TestRegistry::get(std::to_string(i));
-        REQUIRE(std::is_same_v<decltype(instance), std::shared_ptr<Test>>);
-        REQUIRE(instance->value == int(i));
-        REQUIRE(instance->moved);
+        static_assert(std::is_same_v<decltype(instance), std::shared_ptr<Test>>);
+        REQUIRE(instance != nullptr);
+        CHECK(instance->value == i);
+        CHECK(instance->moved);
     }
 
     auto nullInstance = TestRegistry::get("100");
-    REQUIRE(!nullInstance);
+    CHECK(nullInstance == nullptr);
 
     TestRegistry::clear();
     size = TestRegistry::size();
-    REQUIRE(size == 0);
+    CHECK(size == 0);
 }
 
 TEST_CASE("3. GlobalRegistry can hold derived types with abstract interface", "[unit][GlobalRegistry]")
@@ -174,10 +174,50 @@ TEST_CASE("3. GlobalRegistry can hold derived types with abstract interface", "[
         {"instance1", Derived()},
         {"instance2", Derived()}
     });
+
     auto size = BaseRegistry::size();
     REQUIRE(size == 3);
+    for (std::size_t i = 0; i < size; ++i) {
+        auto instance = BaseRegistry::get(std::format("instance{}", i));
+        REQUIRE(instance != nullptr);
+        CHECK(dynamic_cast<Derived*>(instance.get()) != nullptr);
+    }
 
     BaseRegistry::clear();
     size = BaseRegistry::size();
-    REQUIRE(size == 0);
+    CHECK(size == 0);
+}
+
+TEST_CASE("4. Instances can be appended to GlobalRegistry with append()", "[unit][GlobalRegistry]")
+{
+    struct Test {
+        explicit Test(int a)
+            : value(a)
+        {}
+
+        int value{};
+    };
+
+    using TestRegistry = utils::registry::GlobalRegistry<Test, int>;
+
+    TestRegistry::init({
+        {0, Test(0)},
+        {1, Test(1)}
+    });
+    CHECK(TestRegistry::size() == 2);
+
+    TestRegistry::append({
+        {2, Test(2)},
+        {3, Test(3)}
+    });
+    CHECK(TestRegistry::size() == 4);
+
+    for (int i = 0; i < 4; ++i) {
+        auto instance = TestRegistry::get(i);
+        REQUIRE(instance != nullptr);
+        CHECK(instance->value == i);
+    }
+
+    TestRegistry::clear();
+    CHECK(TestRegistry::size() == 0);
 }
